991b3470decf8f805d21b109d2397f738a99c75b
Alexandra Burnside <burnsideally@gmail.com>
2022-03-17 09:53:37 +0000

    add function working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const addOne = () => {
+    setCount(count + 1);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,8 +16,8 @@ function App() {
           <p>Increment and Decrement</p>
           <div className="counter">
             <button>-</button>
-            <span>0</span>
-            <button>+</button>
+            <span>{count}</span>
+            <button onClick={addOne}>+</button>
           </div>
         </div>
       </header>
